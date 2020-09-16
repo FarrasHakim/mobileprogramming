15c68e26ecf7961117ea17ec77bf540440813732
Farras Hakim <farrashakim@gmail.com>
2020-09-16 16:06:29 +0700

    Added project HelloWorld

---
+++ HelloWorld/app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.mobileprogramming.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("Test Debugging", "Hello There. Ini On Create");
+    }
+}
