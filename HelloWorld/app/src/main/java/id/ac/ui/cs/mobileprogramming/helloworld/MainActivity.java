51579f62bab0c80c75f35960304e521a211b37d7
Farras Hakim <farrashakim@gmail.com>
2020-09-21 21:39:20 +0700

    Added button feature for test

--- HelloWorld/app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("Test Debugging", "Hello There. Ini On Create");
+    }
+
+    public void onClick(View v) {
+        Toast toast = Toast.makeText(v.getContext(), "Berhasil di click", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
