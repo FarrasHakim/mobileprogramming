c01ac81930c5e74f7dc20d195caf1576af94bc15
Farras Hakim <farrashakim@gmail.com>
2020-10-21 16:23:07 +0700

    Google fundamentals manual counter

--- HelloWorld/app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.mobileprogramming.helloworld;
 
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,15 +10,27 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MainActivity Debug";
+    private int mCount = 0;
+    private TextView mShowCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Test Debugging", "Hello There. Ini On Create");
+        mShowCount = (TextView) findViewById(R.id.counter_text);
+        Log.d(TAG, "OnCreate()");
     }
 
-    public void onClick(View v) {
-        Toast toast = Toast.makeText(v.getContext(), "Berhasil di click", Toast.LENGTH_SHORT);
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_msg, Toast.LENGTH_SHORT);
         toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
