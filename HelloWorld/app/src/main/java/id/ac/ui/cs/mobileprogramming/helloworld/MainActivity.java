ae8576bb53e1fb36c6ebfd7be7f10517ff66b6d7
Farras Hakim <farrashakim@gmail.com>
2020-10-21 23:28:25 +0700

    Two Activity

--- HelloWorld/app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
@@ -1,5 +1,8 @@
 package id.ac.ui.cs.mobileprogramming.helloworld;
 
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,16 +11,46 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int TEXT_REQUEST = 1;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+    private static final String TAG = "MainActivity Debugging";
+    public static final String EXTRA_MESSAGE =
+            "id.ac.ui.cs.mobileprogramming.extra.MESSAGE";
+    private EditText mMessageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Test Debugging", "Hello There. Ini On Create");
+        mMessageEditText = findViewById(R.id.editText_main);
+        Log.d(TAG, "Ini On Create");
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
+        Log.d(TAG, "Visibilitas:" + mReplyHeadTextView.getVisibility());
     }
 
-    public void onClick(View v) {
-        Toast toast = Toast.makeText(v.getContext(), "Berhasil di click", Toast.LENGTH_SHORT);
-        toast.show();
+    public void launchSecondActivity(View view) {
+        Log.d(TAG, "Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode, Intent data) {
+        Log.d("Main", "Terima Result" + requestCode);
+        if (requestCode == TEXT_REQUEST) {
+            Log.d("Main", "Terima Text Request" + resultCode);
+            if (resultCode == RESULT_OK) {
+                Log.d("Main", "Terima Result OK");
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
---
+++ HelloWorld/app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/SecondActivity.java
@@ -0,0 +1,35 @@
+package id.ac.ui.cs.mobileprogramming.helloworld;
+
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY =
+            "com.example.android.twoactivities.extra.REPLY";
+    private EditText mReply;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+    }
+
+    public void returnReply(View view) {
+        Log.d("Second Activity", "Reply button clicked!");
+        mReply = findViewById(R.id.edit_reply_text);
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK,replyIntent);
+        finish();
+    }
+}
