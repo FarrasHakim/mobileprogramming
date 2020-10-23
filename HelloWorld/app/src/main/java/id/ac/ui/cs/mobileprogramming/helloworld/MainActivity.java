d053232a4ddbc36dfac0ed0b932ee8750711eaff
Farras Hakim <farrashakim@gmail.com>
2020-10-23 10:50:12 +0700

    Droid Cafe Desserts

--- HelloWorld/app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
@@ -1,13 +1,16 @@
 package id.ac.ui.cs.mobileprogramming.helloworld;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private String mOrderMessage;
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.droidcafe.extra.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +19,29 @@ public class MainActivity extends AppCompatActivity {
         Log.d("Test Debugging", "Hello There. Ini On Create");
     }
 
-    public void onClick(View v) {
-        Toast toast = Toast.makeText(v.getContext(), "Berhasil di click", Toast.LENGTH_SHORT);
-        toast.show();
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+        startActivity(intent);
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void showDonutOrder(View view) {
+        mOrderMessage = getString(R.string.donut_order_message);
+        displayToast(mOrderMessage);
+    }
+
+    public void showIceCreamOrder(View view) {
+        mOrderMessage = getString(R.string.ice_cream_order_message);
+        displayToast(mOrderMessage);
+    }
+
+    public void showFroyoOrder(View view) {
+        mOrderMessage = getString(R.string.froyo_order_message);
+        displayToast(mOrderMessage);
     }
 }
---
+++ HelloWorld/app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/OrderActivity.java
@@ -0,0 +1,20 @@
+package id.ac.ui.cs.mobileprogramming.helloworld;
+
+import android.content.Intent;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        Intent intent = getIntent();
+        String message = "Order: " +
+                intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.order_textview);
+        textView.setText(message);
+    }
+}
