ca4221c4f3ac3fb0e715b3baec7db9882caa258d
Farras Hakim <farrashakim@gmail.com>
2020-10-23 20:12:12 +0700

    Menu Picker for Droid Dessert

--- HelloWorld/app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/id/ac/ui/cs/mobileprogramming/helloworld/MainActivity.java
@@ -3,9 +3,12 @@ package id.ac.ui.cs.mobileprogramming.helloworld;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     private String mOrderMessage;
@@ -16,7 +19,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         Log.d("Test Debugging", "Hello There. Ini On Create");
+    }
+
+    /**
+     * Inflates the menu, and adds items to the action bar if it is present.
+     *
+     * @param menu Menu to inflate.
+     * @return Returns true if the menu inflated.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /**
+     * Handles app bar item clicks.
+     *
+     * @param item Item clicked.
+     * @return True if one of the defined items was clicked.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_order:
+                if (mOrderMessage != null) {
+                    Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                    intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                    startActivity(intent);
+                } else {
+                    displayToast("You have to order something.");
+                }
+                return true;
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status_message));
+                return true;
+            case R.id.action_favorites:
+                displayToast(getString(R.string.action_favorites_message));
+                return true;
+            case R.id.action_contact:
+                displayToast(getString(R.string.action_contact_message));
+                return true;
+            default:
+                // Do nothing
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void onClick(View view) {
